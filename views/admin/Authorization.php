c55cf15d8a89f927c9fe52a240294073faf97d0d
hameemvs <1234hameem@.com>
2025-08-14 11:44:52 +0530

    profileImageUpdate

--- views/admin/Authorization.php
+++ views/admin/Authorization.php
@@ -49,7 +49,7 @@ $users = $userDetails->getUserbyStatus();
                             <div class="col-sm-4 text-center">
                                 <div class="card-body p-0 d-flex justify-content-center align-items-center">
                                     <div class="user-photo">
-                                        <img src="<?= !empty($pending['photo']) ? url('uploads/' . $pending['photo']) : url('assets/img/illustrations/mine-strappen.png') ?>"
+                                        <img src="<?= !empty($pending['photo']) ? url($pending['photo']) : url('assets/img/illustrations/mine-strappen.png') ?>"
                                             alt="User Photo"
                                             style="width: 100%; height: 100%; object-fit: cover;" />
                                     </div>
