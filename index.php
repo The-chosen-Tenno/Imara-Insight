465870a7c37859d11191d8070c5901a0a7577f01
The-chosen-Tenno <xempforever@gmail.com>
2025-11-11 11:08:33 +0530

    fixing small login redirect issue for admin

--- index.php
+++ index.php
@@ -10,11 +10,6 @@ if (empty($username)) {
     $sm->destroy();  // use SessionManager to destroy session
     header('Location: views/auth/login.php');
     exit;
-}
-
-if ($role === "admin") {
-    header('Location: views/system/Authorization.php');
-    exit;
 } else {
     header('Location: views/system/dashboard.php');
     exit;
